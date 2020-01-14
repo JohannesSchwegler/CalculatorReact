d261b1f6977fe4fd5b1d4198a787eed35ab6b3d7
Johannes Schwegler <johannes.schwegler@outlook.de>
2020-01-14 22:49:27 +0100

    first commit

---
+++ precache-manifest.60c62e27fb5bc82269092bef0e66983a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce22aa4eff492745f858c5fc7a01ac0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b78aea6df5c08db459f",
+    "url": "/static/css/main.72398cbc.chunk.css"
+  },
+  {
+    "revision": "c55e40f6ddb737d2d6b0",
+    "url": "/static/js/2.3ac77407.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3ac77407.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b78aea6df5c08db459f",
+    "url": "/static/js/main.b1c4d3f8.chunk.js"
+  },
+  {
+    "revision": "77698a1cc3b108e89cf7",
+    "url": "/static/js/runtime-main.1cff83f8.js"
+  }
+]);
